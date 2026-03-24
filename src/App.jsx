43eae17e3d18ca81ac7e6461d83407ebafbe9527
Alexander Mai <alexandermai94@live.de>
2026-03-24 22:51:18 +0100

    add comment

--- src/App.jsx
+++ src/App.jsx
@@ -86,59 +86,59 @@ function App() {
       <div className='w-full h-full absolute left-0 top-0 text-amber-200'>
         <h1 className='text-4xl text-center p-5 font-mono'>Character Sheet</h1>
         <hr />
-        <section>
-          {/* id: "hero_9921" */}
-          {/* metadata: {
-            name: "Valerius the Bold",
-            class: "Paladin",
-            race: "Human",
-            level: 14,
-            alignment: "Lawful Good"
-          } */}
-          <table>
-            <tbody>
-              <tr><td>Name</td><td>{rpgCharacter.metadata.name}</td></tr>
-              <tr><td>Class</td><td>{rpgCharacter.metadata.class}</td></tr>
-              <tr><td>Race</td><td>{rpgCharacter.metadata.race}</td></tr>
-              <tr><td>Level</td><td>{rpgCharacter.metadata.level}</td></tr>
-            </tbody>
-          </table>
-          {/* // Primary attributes
-            stats: {
-              strength: 18,
-              dexterity: 10,
-              constitution: 16,
-              intelligence: 12,
-              wisdom: 14,
-              charisma: 16
-          } */}
-          <table>
-            <tbody>
-              <tr><td>strength</td><td>{rpgCharacter.stats.strength}</td></tr>
-              <tr><td>dexterity</td><td>{rpgCharacter.stats.dexterity}</td></tr>
-              <tr><td>constitution</td><td>{rpgCharacter.stats.constitution}</td></tr>
-              <tr><td>intelligence</td><td>{rpgCharacter.stats.intelligence}</td></tr>
-              <tr><td>wisdom</td><td>{rpgCharacter.stats.wisdom}</td></tr>
-              <tr><td>charisma</td><td>{rpgCharacter.stats.charisma}</td></tr>
-            </tbody>
-          </table>
-  
-          {/* // Current status
-          status: {
-            hp: { current: 112, max: 125 },
-            mp: { current: 45, max: 60 },
-            stamina: 88,
-            isResting: false,
-            activeEffects: ["Blessing of Light", "Encumbered"]
-          } */}
-          <table>
-            <tbody>
-              <tr><td>Health</td><td>{rpgCharacter.status.hp.current}</td><td>/</td><td>{rpgCharacter.status.hp.max}</td></tr>
-              <tr><td>Mana</td><td>{rpgCharacter.status.mp.current}</td><td>/</td><td>{rpgCharacter.status.mp.max}</td></tr>
-              <tr><td>Stamina</td><td>{rpgCharacter.status.sp.current}</td><td>/</td><td>{rpgCharacter.status.sp.max}</td></tr>
-            </tbody>
-          </table>
-        </section>
+
+        {/* Each table should be a section and contain a React component/composition for the relevant data block */}
+        {/* id: "hero_9921" */}
+        {/* metadata: {
+          name: "Valerius the Bold",
+          class: "Paladin",
+          race: "Human",
+          level: 14,
+          alignment: "Lawful Good"
+        } */}
+        <table>
+          <tbody>
+            <tr><td>Name</td><td>{rpgCharacter.metadata.name}</td></tr>
+            <tr><td>Class</td><td>{rpgCharacter.metadata.class}</td></tr>
+            <tr><td>Race</td><td>{rpgCharacter.metadata.race}</td></tr>
+            <tr><td>Level</td><td>{rpgCharacter.metadata.level}</td></tr>
+          </tbody>
+        </table>
+        {/* // Primary attributes
+          stats: {
+            strength: 18,
+            dexterity: 10,
+            constitution: 16,
+            intelligence: 12,
+            wisdom: 14,
+            charisma: 16
+        } */}
+        <table>
+          <tbody>
+            <tr><td>strength</td><td>{rpgCharacter.stats.strength}</td></tr>
+            <tr><td>dexterity</td><td>{rpgCharacter.stats.dexterity}</td></tr>
+            <tr><td>constitution</td><td>{rpgCharacter.stats.constitution}</td></tr>
+            <tr><td>intelligence</td><td>{rpgCharacter.stats.intelligence}</td></tr>
+            <tr><td>wisdom</td><td>{rpgCharacter.stats.wisdom}</td></tr>
+            <tr><td>charisma</td><td>{rpgCharacter.stats.charisma}</td></tr>
+          </tbody>
+        </table>
+
+        {/* // Current status
+        status: {
+          hp: { current: 112, max: 125 },
+          mp: { current: 45, max: 60 },
+          stamina: 88,
+          isResting: false,
+          activeEffects: ["Blessing of Light", "Encumbered"]
+        } */}
+        <table>
+          <tbody>
+            <tr><td>Health</td><td>{rpgCharacter.status.hp.current}</td><td>/</td><td>{rpgCharacter.status.hp.max}</td></tr>
+            <tr><td>Mana</td><td>{rpgCharacter.status.mp.current}</td><td>/</td><td>{rpgCharacter.status.mp.max}</td></tr>
+            <tr><td>Stamina</td><td>{rpgCharacter.status.sp.current}</td><td>/</td><td>{rpgCharacter.status.sp.max}</td></tr>
+          </tbody>
+        </table>
       </div>
     </PixelCard>
 
