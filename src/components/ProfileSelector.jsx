b78a5c7159d3e5817df3581fae7a960eb7844412
Alexander Mai <alexandermai94@live.de>
2026-03-25 20:13:19 +0100

    expanded Detail View

--- src/components/ProfileSelector.jsx
+++ src/components/ProfileSelector.jsx
@@ -23,19 +23,32 @@ function ProfileSelector() {
   return (
     <PixelCard variant='pink' className='border-amber-200 text-amber-200'>
       {!showDetail ? <ul className='p-10'>
-        {users.map((user, i) => <li key={user.id}>
-          <button onClick={() => { showUserDetail(user) }}>{user.name} 🔍</button>
+        {users.map((user, i) => <li key={user.id} className='
+          border-2 border-amber-250 rounded-md
+          p-2 m-1 
+          bg-violet-500
+          hover:bg-violet-800
+        '>
+          <button onClick={() => { showUserDetail(user) }}>🔍 {user.name} 🔍</button>
         </li>)}
       </ul> : <section className='p-10'>
-        { currentUser !== null ? <section className=''>
+        { currentUser !== null ? <section className='
+          border-2 border-amber-250 rounded-md
+          p-2 m-1 
+          bg-violet-500
+          hover:bg-violet-800'>
           {/* Detail view for profile */}
-          <h2>{currentUser.name}</h2>
+          <h2>Clone Nr. {currentUser.id} // {currentUser.username}</h2>
           <hr />
+          <h3>{currentUser.name}</h3>
+          <h3>{currentUser.email}</h3>
+          <h3>{currentUser.phone}</h3>
+          <h3>{currentUser.website}</h3>
           
-          <button onClick={() => { setCurrentUser(null); setShowDetail(false) }}>Close Detail</button>
+          <button onClick={() => { setCurrentUser(null); setShowDetail(false) }}>❌ Close Detail ❌</button>
         </section> : <section className=''>
           <p>Something went wrong!</p>
-          <button onClick={() => { setCurrentUser(null); setShowDetail(false) }}>Close View</button>
+          <button onClick={() => { setCurrentUser(null); setShowDetail(false) }}>❌ Close View ❌</button>
         </section> }
       </section>}
     </PixelCard>
